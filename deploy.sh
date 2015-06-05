bf7eb8874c18a90cce31a46369254fc4342f4867
Lucas Pirola <lucaspirola@gmail.com>
2015-06-05 11:45:49 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ssh -p 6666 hacker@nucleodigital.cc 'rm -rf /home/user-data/www/default/*'
-scp -rp -P6666 ~/nucleo-digital.github.io/_site/* hacker@nucleodigital.cc:/home/user-data/www/default/
+scp -rp -P6666 _site/* hacker@nucleodigital.cc:/home/user-data/www/default/
